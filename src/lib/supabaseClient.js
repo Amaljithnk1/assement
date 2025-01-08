f86e6742aef89f1da7464a45708f4d3795d8380b
Amaljithnk1 <amajithnk@gmail.com>
2025-01-08 14:32:07 +0530

    5th commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,7 @@
-// lib/supabaseClient.ts
+// lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL  ,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ,
-  
-
-
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 // _app.js
-import '@/styles/globals.css'; // Ensure you have your TailwindCSS imported
+import '@/styles/globals.css'; 
 import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    // Any client-side setup (e.g., theme initialization)
+    
   }, []);
 
   return <Component {...pageProps} />;
